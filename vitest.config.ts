8f9b3d9effe090ba0ad8afc33bc67bc44e2e504b
godlikeAlex <godlikedesigner@gmail.com>
2025-05-01 01:32:09 +0500

    chore: add vitest/coverage-v8 for coverage tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,9 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: 'text',
+    },
   },
 });
