153e962eb6a67dbc1714968889748f251261ea8b
godlikeAlex <godlikedesigner@gmail.com>
2025-04-30 19:06:22 +0500

    feat: add react-router and basic 404page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-import { Home } from "./pages";
+import { Route, Routes, BrowserRouter } from 'react-router';
+import { Home, NotFound } from './pages';
 
-const App = () => <Home />;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
