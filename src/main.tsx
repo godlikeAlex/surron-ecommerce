2424aac6e9c4a9f4c724b85a78800808d35206c4
godlikeAlex <godlikedesigner@gmail.com>
2025-05-01 01:14:38 +0500

    chore: add mantine and basic config for it

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { MantineProvider } from '@mantine/core';
+import { theme } from './theme';
+
+import '@mantine/core/styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <App />
+    </MantineProvider>
   </StrictMode>
 );
