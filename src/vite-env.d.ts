7ca34990d6e38ef2d717b02c3aab1f39a8e13221
godlikeAlex <godlikedesigner@gmail.com>
2025-05-01 02:33:09 +0500

    refactor: change env host to region

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  VAR_COMMERCE_TOOLS_HOST: string;
   VAR_COMMERCE_TOOLS_PROJECT_KEY: string;
   VAR_COMMERCE_TOOLS_CLIENT_ID: string;
   VAR_COMMERCE_TOOLS_CLIENT_SECRET: string;
+  VAR_COMMERCE_TOOLS_REGION: string;
 }
 
 interface ImportMeta {
