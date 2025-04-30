81b180d6d7146e4e392a8fb717a25f2a03519270
godlikeAlex <godlikedesigner@gmail.com>
2025-05-01 02:17:49 +0500

    chore: add .env support and example of .env file

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VAR_COMMERCE_TOOLS_HOST: string;
+  VAR_COMMERCE_TOOLS_PROJECT_KEY: string;
+  VAR_COMMERCE_TOOLS_CLIENT_ID: string;
+  VAR_COMMERCE_TOOLS_CLIENT_SECRET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  envPrefix: 'VAR_',
   test: {
     environment: 'jsdom',
     setupFiles: './src/tests/setup.ts',
