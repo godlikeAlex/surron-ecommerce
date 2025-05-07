887bb5c9e3ee2b70cab5290e0a402ae229f22f83
godlikeAlex <godlikedesigner@gmail.com>
2025-05-07 14:13:07 +0500

    refactor: switch to using plugins via extends

--- eslint.config.js
+++ eslint.config.js
@@ -13,38 +13,26 @@ import jsxA11y from 'eslint-plugin-jsx-a11y';
 export default tseslint.config(
   { ignores: ['dist', 'coverage'] },
   {
+    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat['jsx-runtime'],
+      jsxA11y.flatConfigs.recommended,
+      reactHooks.configs['recommended-latest'],
+      pluginQuery.configs['flat/recommended'],
+      reactRefresh.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
     ],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
-        ecmaFeatures: { jsx: true },
       },
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    plugins: {
-      react: reactPlugin,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'jsx-a11y': jsxA11y,
-      '@tanstack/query': pluginQuery,
     },
     rules: {
-      ...reactPlugin.configs.recommended.rules,
-      ...reactPlugin.configs.flat['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      ...jsxA11y.configs.recommended.rules,
-      ...pluginQuery.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
