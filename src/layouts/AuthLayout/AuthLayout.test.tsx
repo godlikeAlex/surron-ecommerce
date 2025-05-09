5cb57cc64cfc9e47f73e20ec5c4c78913a4b596c
godlikeAlex <godlikedesigner@gmail.com>
2025-05-09 23:28:20 +0500

    feat: add tests for AuthLayout and AuthCard

---
+++ src/layouts/AuthLayout/AuthLayout.test.tsx
@@ -0,0 +1,64 @@
+import { describe, expect, it } from 'vitest';
+import AuthLayout from './AuthLayout';
+import { render, screen } from '@/tests/utils';
+
+import authBg1 from '@/assets/backgrounds/auth-bg-1.webp';
+
+describe('component AuthLayout', () => {
+  it('should render with correct children', () => {
+    expect.hasAssertions();
+
+    const children = 'Hello world';
+
+    render(<AuthLayout backgroundVariant="first">{children}</AuthLayout>);
+
+    expect(screen.getByText(children)).toBeInTheDocument();
+  });
+
+  it('should render with correct background', () => {
+    expect.hasAssertions();
+
+    render(
+      <AuthLayout backgroundVariant="first">
+        <form name="registration"></form>
+      </AuthLayout>
+    );
+
+    const childrenLayout = screen.getByRole('form');
+
+    expect(childrenLayout.parentElement).toHaveStyle(
+      `background-image: url(${authBg1})`
+    );
+  });
+});
+
+describe('component AuthCard', () => {
+  it('should render with correct title text', () => {
+    expect.hasAssertions();
+
+    const title = 'Login Page';
+
+    render(
+      <AuthLayout.Card title={title}>
+        <form></form>
+      </AuthLayout.Card>
+    );
+
+    const heading = screen.getByRole('heading', { level: 1 });
+
+    expect(heading).toBeInTheDocument();
+    expect(heading.textContent).equal(title);
+  });
+
+  it('should render with correct children', () => {
+    expect.hasAssertions();
+
+    render(
+      <AuthLayout.Card title={'Registration Form'}>
+        <form name="registration"></form>
+      </AuthLayout.Card>
+    );
+
+    expect(screen.getByRole('form')).toBeInTheDocument();
+  });
+});
