e5121842a6466ff043de19430e8068b18589266f
Lorenzo-StJohn <britishaccent@bk.ru>
2025-05-10 05:17:29 +0400

    feat: implement class for saving tokens

---
+++ src/api/MyTokenCache.ts
@@ -0,0 +1,21 @@
+import { TokenCache, TokenStore } from '@commercetools/ts-client';
+
+export class MyTokenCache implements TokenCache {
+  #tokenStore: TokenStore;
+
+  constructor() {
+    this.#tokenStore = {
+      expirationTime: 0,
+      refreshToken: undefined,
+      token: '',
+    };
+  }
+
+  public get() {
+    return this.#tokenStore;
+  }
+
+  public set(tokenStore: TokenStore) {
+    this.#tokenStore = tokenStore;
+  }
+}
