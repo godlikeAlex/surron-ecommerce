0def07fdb41e7ef1049b234790fc9d30a6d86def
godlikeAlex <godlikedesigner@gmail.com>
2025-05-11 18:06:10 +0500

    feat: add tests for Registration page

--- src/pages/Registration/Registration.tsx
+++ src/pages/Registration/Registration.tsx
@@ -1,12 +1,10 @@
 import { RegistrationForm } from './components/RegistrationForm';
 import { AuthLayout } from '@/layouts';
 
-const Registration = () => (
+export const Registration = () => (
   <AuthLayout backgroundVariant="first">
     <AuthLayout.Card title="Регистрация">
       <RegistrationForm />
     </AuthLayout.Card>
   </AuthLayout>
 );
-
-export default Registration;
