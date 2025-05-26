e7bc2a8fd636ff7ba783e21698cd3bd77f765a83
godlikeAlex <godlikedesigner@gmail.com>
2025-05-26 13:39:02 +0500

    feat: add links to breadcrumbs

--- src/pages/Catalog/components/BreadcrumbsCategories/BreadcrumbsCategories.tsx
+++ src/pages/Catalog/components/BreadcrumbsCategories/BreadcrumbsCategories.tsx
@@ -1,20 +1,50 @@
 import { Anchor, Breadcrumbs, Text } from '@mantine/core';
 import { Category } from '@/pages/Catalog/hooks/useCategories';
+import { Link } from 'react-router';
+import { useMemo } from 'react';
 
 type Props = {
   currentCategories: Category[];
 };
 
+const DEFAULT_BREADCRUMBS = [
+  {
+    name: 'Главная',
+    slug: '/',
+  },
+  {
+    name: 'Каталог',
+    slug: '/catalog',
+  },
+];
+
 export const BreadcrumbsCategories = ({ currentCategories }: Props) => {
+  const breadcrumbs = useMemo(() => {
+    const breadCrumbsWithSlugs = currentCategories.map((category, index) => {
+      const slugsBeforeCurrent = currentCategories.slice(0, index + 1);
+
+      const chainUrl = slugsBeforeCurrent
+        .map((category) => category.slug)
+        .join('/');
+
+      return {
+        name: category.name,
+        slug: `/catalog/${chainUrl}`,
+      };
+    });
+
+    return [...DEFAULT_BREADCRUMBS, ...breadCrumbsWithSlugs];
+  }, [currentCategories]);
+
   return (
     <Breadcrumbs>
-      <Anchor>Главная</Anchor>
-      <Anchor>Каталог</Anchor>
-      {currentCategories.map((category, index) =>
-        index === currentCategories.length - 1 ? (
-          <Text key={category.slug}>{category.name}</Text>
+      {breadcrumbs.map((breadcrumb, index) =>
+        index === breadcrumbs.length - 1 ? (
+          <Text key={breadcrumb.slug}>{breadcrumb.name}</Text>
         ) : (
-          <Anchor key={category.slug}>{category.name}</Anchor>
+          <Anchor key={breadcrumb.slug} component={Link} to={breadcrumb.slug}>
+            {breadcrumb.name}
+          </Anchor>
         )
       )}
     </Breadcrumbs>
