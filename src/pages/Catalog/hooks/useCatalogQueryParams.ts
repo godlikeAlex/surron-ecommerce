9f3c1d531654b9fdc8d327eaf64a1e5044df1bdc
godlikeAlex <godlikedesigner@gmail.com>
2025-05-30 00:07:16 +0500

    feat: add price range to url after apply filters

--- src/pages/Catalog/hooks/useCatalogQueryParams.ts
+++ src/pages/Catalog/hooks/useCatalogQueryParams.ts
@@ -1,14 +1,28 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import { useSearchParams } from 'react-router';
 
 type CatalogQueryParams = {
-  page: number;
+  page?: number;
+  rangePrice?: [number, number];
 };
 
 export const useCatalogQueryParams = () => {
   const [searchParmas, setSearchParams] = useSearchParams();
 
   const page = searchParmas.get('page') || 1;
+  const priceRange = searchParmas.get('price-range');
+
+  const parsedPriceRange = useMemo(() => {
+    if (!priceRange) return;
+
+    const [from, to] = priceRange.split('-');
+
+    if (!from || !to) {
+      return;
+    }
+
+    return { from: Number(from), to: Number(to) };
+  }, [priceRange]);
 
   const setCatalogQueryParams = useCallback(
     (catalogQueryParms: CatalogQueryParams) => {
@@ -17,11 +31,21 @@ export const useCatalogQueryParams = () => {
           currentParams.set('page', String(catalogQueryParms.page));
         }
 
+        if (catalogQueryParms.rangePrice) {
+          const [from, to] = catalogQueryParms.rangePrice;
+
+          currentParams.set('price-range', `${from}-${to}`);
+        }
+
         return currentParams;
       });
     },
-    []
+    [setSearchParams]
   );
 
-  return { page: Number(page), setCatalogQueryParams };
+  return {
+    page: Number(page),
+    priceRange: parsedPriceRange,
+    setCatalogQueryParams,
+  };
 };
