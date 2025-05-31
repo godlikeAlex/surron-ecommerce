b91d6fc92eba47653b1ad61301b7ed714cc86cf5
godlikeAlex <godlikedesigner@gmail.com>
2025-06-01 01:10:52 +0500

    feat: fetch products by color and price

--- src/pages/Catalog/components/SidebarFilters/SidebarSection.tsx
+++ src/pages/Catalog/components/SidebarFilters/SidebarSection.tsx
@@ -1,25 +1,35 @@
 import { ReactNode } from 'react';
-import { Box, Title } from '@mantine/core';
+import { Box, Skeleton, Title } from '@mantine/core';
 import classes from './SidebarFilters.module.scss';
 
 type Props = {
-  title: string;
+  title?: string;
   children: ReactNode;
   withPadding?: boolean;
 };
 
-export const SidebarSection = ({
-  title,
-  withPadding = true,
-  children,
-}: Props) => (
+const SidebarSection = ({ title, withPadding = true, children }: Props) => (
   <Box className={classes.sidebarSection}>
-    <Title order={5} className={classes.sidebarItemWrapper}>
-      {title}
-    </Title>
+    {title && (
+      <Title order={5} className={classes.sidebarItemWrapper}>
+        {title}
+      </Title>
+    )}
 
     <Box className={withPadding ? classes.sidebarItemWrapper : undefined}>
       {children}
     </Box>
   </Box>
 );
+
+const SidebarSectionSkeleton = ({ children }: { children: ReactNode }) => (
+  <SidebarSection title="">
+    <Skeleton w={145} h={25} mb={10} />
+
+    {children}
+  </SidebarSection>
+);
+
+SidebarSection.Skeleton = SidebarSectionSkeleton;
+
+export { SidebarSection };
