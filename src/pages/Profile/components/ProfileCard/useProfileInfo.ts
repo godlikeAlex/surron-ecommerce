fd7ae7f357d41a1ff69f669ab3abe5bfba3c1684
Lorenzo-StJohn <britishaccent@bk.ru>
2025-06-02 09:18:02 +0400

    feat: implement editing personal info

--- src/pages/Profile/components/ProfileCard/useProfileInfo.ts
+++ src/pages/Profile/components/ProfileCard/useProfileInfo.ts
@@ -1,7 +1,11 @@
 import { apiRootStore, useApiRootStore } from '@/store/apiRootStore';
+import { UseFormReturnType } from '@mantine/form';
 import { useQuery } from '@tanstack/react-query';
+import { InfoValues } from './ProfileCard';
 
-export const useProfileInfo = () => {
+export const useProfileInfo = (
+  setFormInfo: UseFormReturnType<InfoValues, (values: InfoValues) => InfoValues>
+) => {
   const apiRoot = useApiRootStore((state) => state.apiRoot);
   const token = useApiRootStore((state) => state.refreshToken);
   const version = useApiRootStore((state) => state.version);
@@ -11,6 +15,9 @@ export const useProfileInfo = () => {
     queryFn: async () => {
       const response = await apiRoot.me().get().execute();
       apiRootStore().setVersion(response.body.version);
+      setFormInfo.setFieldValue('firstName', response.body.firstName || '');
+      setFormInfo.setFieldValue('lastName', response.body.lastName || '');
+      setFormInfo.setFieldValue('dateOfBirth', response.body.dateOfBirth || '');
       return response;
     },
   });
