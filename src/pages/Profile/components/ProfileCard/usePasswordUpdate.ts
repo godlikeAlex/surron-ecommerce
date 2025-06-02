f72980efcd770065907e4b7b49ae6768cf531fb3
Lorenzo-StJohn <britishaccent@bk.ru>
2025-06-02 11:56:56 +0400

    refactor: catch error in case of invalid password

--- src/pages/Profile/components/ProfileCard/usePasswordUpdate.ts
+++ src/pages/Profile/components/ProfileCard/usePasswordUpdate.ts
@@ -9,36 +9,37 @@ export const usePasswordChange = () => {
 
   const { isPending, mutateAsync } = useMutation({
     mutationFn: async (action: CustomerChangePassword) => {
-      const response = await apiRoot
-        .me()
-        .password()
-        .post({
-          body: action,
-        })
-        .execute();
-      if (response.statusCode === 200)
-        setLogin(response.body.email, action.newPassword);
-      apiRootStore().setVersion(response.body.version);
-    },
-    onSuccess: () => {
-      notifications.show({
-        title: 'Поздравляем!',
-        message: 'Пароль успешно обновлён',
-        autoClose: 7000,
-        withCloseButton: true,
-        withBorder: true,
-        color: 'green',
-      });
-    },
-    onError: () => {
-      notifications.show({
-        title: 'Упс!',
-        message: 'Не удалось обновить пароль',
-        autoClose: 7000,
-        withCloseButton: true,
-        withBorder: true,
-        color: 'red',
-      });
+      try {
+        const response = await apiRoot
+          .me()
+          .password()
+          .post({
+            body: action,
+          })
+          .execute();
+        if (response.statusCode === 200) {
+          setLogin(response.body.email, action.newPassword);
+          apiRootStore().setVersion(response.body.version);
+          notifications.show({
+            title: 'Поздравляем!',
+            message: 'Пароль успешно обновлён',
+            autoClose: 7000,
+            withCloseButton: true,
+            withBorder: true,
+            color: 'green',
+          });
+        }
+      } catch (error) {
+        if (error) console.log();
+        notifications.show({
+          title: 'Упс!',
+          message: 'Не удалось обновить пароль',
+          autoClose: 7000,
+          withCloseButton: true,
+          withBorder: true,
+          color: 'red',
+        });
+      }
     },
   });
 
