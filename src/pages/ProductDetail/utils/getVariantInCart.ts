284def981cdbb1d693c188e1b0ce671a6749a7e5
Milena Belianova <milena.belianova@gmail.com>
2025-06-12 15:14:02 +0400

    feat: add remove from cart logic

--- src/pages/ProductDetail/utils/getVariantInCart.ts
+++ src/pages/ProductDetail/utils/getVariantInCart.ts
@@ -1,12 +1,13 @@
 import { Cart, ProductVariant } from '@commercetools/platform-sdk';
 import { ProductType } from './parseProductData';
 
-export const isVariantInCart = (
+export const getVariantInCart = (
   cart: Cart,
   product: ProductType,
   variant: ProductVariant
 ) => {
-  return cart.lineItems.some(
+  const matchingItem = cart.lineItems.find(
     (item) => item.productId === product.id && item.variant.id === variant.id
   );
+  return matchingItem;
 };
