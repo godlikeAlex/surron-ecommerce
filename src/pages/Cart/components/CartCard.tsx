009da2948e9c1a38217c60b9f558162ae5ce7a5e
Lorenzo-StJohn <britishaccent@bk.ru>
2025-06-12 07:48:37 +0400

    feat: implement changing quantity in cart

--- src/pages/Cart/components/CartCard.tsx
+++ src/pages/Cart/components/CartCard.tsx
@@ -1,6 +1,8 @@
 import {
+  ActionIcon,
   Avatar,
   Center,
+  Group,
   NumberFormatter,
   ScrollArea,
   Skeleton,
@@ -10,21 +12,24 @@ import {
 } from '@mantine/core';
 import { useGetCart } from '../hooks/useGetCart';
 import classes from './CartCard.module.scss';
-import { Cart } from '@commercetools/platform-sdk';
+import { Cart, MyCartUpdateAction } from '@commercetools/platform-sdk';
 import { useState } from 'react';
 import cx from 'clsx';
 import { useMediaQuery } from '@mantine/hooks';
 import { Link } from 'react-router';
+import { IconMinus, IconPlus } from '@tabler/icons-react';
+import { useQuantity } from '../hooks/useQuantity';
 
 export const CartCard = () => {
   const [scrolled, setScrolled] = useState(false);
   const { isPending, data } = useGetCart();
+  const { isPending: isPendingEdit, mutateAsync } = useQuantity();
 
   const theme = useMantineTheme();
   const isLargeScreen = useMediaQuery(`(min-width: ${theme.breakpoints.lg})`);
 
-  const f = (d: Cart[]) => {
-    const cart = d[0].lineItems;
+  const tableData = (responseCart: Cart[]) => {
+    const cart = responseCart[0].lineItems;
     const rows = cart.map((row) => (
       <Table.Tr key={row.id} className={classes.tableRow}>
         <Table.Td>
@@ -41,19 +46,53 @@ export const CartCard = () => {
           <NumberFormatter
             value={row.price.value.centAmount / 100 || 0}
             thousandSeparator
-            suffix=" ₽"
           />
         </Table.Td>
         <Table.Td>
-          <Text truncate className={classes.quantity}>
-            {row.quantity}
-          </Text>
+          <Group gap={2}>
+            <ActionIcon
+              size={isLargeScreen ? 21 : 16}
+              variant="default"
+              disabled={row.quantity <= 1}
+              onClick={() => {
+                const actions: MyCartUpdateAction[] = [
+                  {
+                    action: 'changeLineItemQuantity',
+                    lineItemId: row.id,
+                    quantity: row.quantity - 1,
+                  },
+                ];
+                void mutateAsync(actions);
+              }}
+            >
+              <IconMinus />
+            </ActionIcon>
+            <Text className={classes.quantity} truncate>
+              {row.quantity}
+            </Text>
+            <ActionIcon
+              size={isLargeScreen ? 21 : 16}
+              variant="default"
+              disabled={row.quantity >= 100}
+              onClick={() => {
+                const actions: MyCartUpdateAction[] = [
+                  {
+                    action: 'changeLineItemQuantity',
+                    lineItemId: row.id,
+                    quantity: row.quantity + 1,
+                  },
+                ];
+                void mutateAsync(actions);
+              }}
+            >
+              <IconPlus />
+            </ActionIcon>
+          </Group>
         </Table.Td>
         <Table.Td>
           <NumberFormatter
             value={row.totalPrice.centAmount / 100 || 0}
             thousandSeparator
-            suffix=" ₽"
           />
         </Table.Td>
       </Table.Tr>
@@ -63,16 +102,16 @@ export const CartCard = () => {
         h={300}
         onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
       >
-        <Table>
+        <Table horizontalSpacing={isLargeScreen ? 'md' : '3px'}>
           <Table.Thead
             className={cx(classes.header, { [classes.scrolled]: scrolled })}
           >
             <Table.Tr>
               <Table.Th>{isLargeScreen ? 'Изображение' : '🛍️'}</Table.Th>
               <Table.Th>{isLargeScreen ? 'Наименование' : '🏷️'}</Table.Th>
-              <Table.Th>{isLargeScreen ? 'Цена' : '💰'}</Table.Th>
+              <Table.Th>{isLargeScreen ? 'Цена, ₽' : '💰, ₽'}</Table.Th>
               <Table.Th>{isLargeScreen ? 'Количество' : '📜'}</Table.Th>
-              <Table.Th>{isLargeScreen ? 'Стоимость' : '💲'}</Table.Th>
+              <Table.Th>{isLargeScreen ? 'Стоимость, ₽' : '💲, ₽'}</Table.Th>
             </Table.Tr>
           </Table.Thead>
           <Table.Tbody>{rows}</Table.Tbody>
@@ -81,7 +120,10 @@ export const CartCard = () => {
     );
   };
   return (
-    <Skeleton visible={isPending} className={classes.cartContainer}>
+    <Skeleton
+      visible={isPending || isPendingEdit}
+      className={classes.cartContainer}
+    >
       {!data || data.length === 0 || data[0].lineItems.length == 0 ? (
         <>
           <Text ta="center" size="md" lh={2}>
@@ -94,7 +136,7 @@ export const CartCard = () => {
           </Center>
         </>
       ) : (
-        f(data)
+        tableData(data)
       )}
     </Skeleton>
   );
--- src/pages/Cart/hooks/useGetCart.ts
+++ src/pages/Cart/hooks/useGetCart.ts
@@ -1,15 +1,21 @@
-import { useApiRootStore } from '@/store/apiRootStore';
-//import { MyCartUpdateAction } from '@commercetools/platform-sdk';
+import { apiRootStore, useApiRootStore } from '@/store/apiRootStore';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetCart = () => {
   const apiRoot = useApiRootStore((state) => state.apiRoot);
-  const token = useApiRootStore((state) => state.refreshToken);
+  const cartId = useApiRootStore((state) => state.cartId);
+  const cartVersion = useApiRootStore((state) => state.cartVersion);
 
   const { data, isPending } = useQuery({
-    queryKey: ['get-cart', token],
+    queryKey: ['get-cart', cartId, cartVersion],
     queryFn: async () => {
       const response = await apiRoot.me().carts().get().execute();
+      apiRootStore().setCartId(
+        response.body.results.length ? response.body.results[0].id : undefined
+      );
+      apiRootStore().setCartVersion(
+        response.body.results.length ? response.body.results[0].version : 0
+      );
       return response.body.results;
     },
   });
